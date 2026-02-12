d5680b3d58c0c8a2409160df6147648d56842fda
Tagdew <robmcbobrob@gmail.com>
2026-02-12 10:59:47 -0600

    Updating Android SDK from 35 to 36. Expermenting with horizontal scroll views and ui to get a sense of what I am doing. along with notes for what i am wanting to do.

--- app/src/main/java/com/robomonobo/yournotes/MainActivity.kt
+++ app/src/main/java/com/robomonobo/yournotes/MainActivity.kt
@@ -3,15 +3,20 @@ package com.robomonobo.yournotes
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.robomonobo.yournotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    protected var appBarNavigationArray = setOf(
+        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.scrollingBottomMenuFragment
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +24,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: Toolbar = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
+
+        val appBarConfiguration = AppBarConfiguration(appBarNavigationArray)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
